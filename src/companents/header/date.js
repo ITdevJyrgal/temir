ed0a4e247dff8c1c7bf2759afa90b83f5b3a1ce9
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 <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>
2022-01-23 18:28:44 +0600

    first commit

---
+++ src/companents/header/date.js
@@ -0,0 +1,56 @@
+const Date = [
+    {
+        id: 1,
+        title: "How to setup my TEMIR card?",
+        item: "Once you receive your card, you can tap to activate and create your account. No application needet and setup takes less than 2 minutes At the time of your order, we only need to know what to print on your card. Everything else, you get to add to your account yourself."
+    },
+
+    {
+        id: 2,
+        title: "What material is TEMIR card?",
+        item: "TEMIR Card  made with  stainless steel. Our cards are sturdy and yet luxury. In addition, the gold, silver, rose gold and black matte finish adds  a premium feel that is incomparable to most cards in the market today."
+    },
+
+    {
+        id: 3,
+        title: "Can I design my Temir card?",
+        item: "Yes! We have an amazing graphic design team in-house that will ensure your logo and artwork translate perfectly into the card. Design your card here .",
+        item2: "Shortly after you place your order, we will provide you with mockup designs to review and choose from. Please make sure you provide us with high quality files in Vector, Ai, PSD, EPS, PDF, JPEG or PNG for the best result possible",
+        item3: "Design limitations:",
+        item4: " We can print by laser Silver on matte black cards You can only design the front of your card"
+    },
+    {
+        id: 4,
+        title: "What is the return policy?",
+        item: "You can get a full refund within 30 days of your purchase date. You don’t need to ship your card back to us, we will deactivate your TEMIR card remotely. You can get a full refund within 30 days of your purchase date. You don’t need to ship your card back to us, we will deactivate your TEMIR card remotely."
+
+    },
+    {
+        id: 5,
+        title: "How long is the delivery?",
+        item: "Orders are processed daily.",
+        item5: " Dubai-Sharjah-Ajman 2-10 hours",
+        item6: " Abu Dhabi-Ras Al Khaimah   1-2 days",
+        item7: "Umm Al Quwain Fujairah      2-3 days"
+    },
+    {
+        id: 6,
+        title: "What is TEMIR’s privacy policy?",
+        item: "We don’t ask our users for any sensitive information nor their social media logins. You can share as much or as little information as you’d like on your TEMIR profile. TEMIR profiles are public landing pages that hosts your information and make it easily sharable to people you meet.",
+        item2: "Read our Privacy Policy and Terms of Agreement",
+    },
+    {
+        id: 7,
+        title: "Any additional charges?",
+        item5: " No!",
+        item6: "Unlike other brands, we have one time payment option only.",
+    },
+
+
+
+
+
+
+]
+
+export default Date
